284db6dd2457803789ba34d6fedf3307f7e1b36c
joybou <ican.notfound@gmial.com>
2022-08-04 09:43:29 +0800

    auth flask coba

---
+++ project/auth.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from . models import User
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+@auth.route('/login' methods=['POST'])
+def login_post():
+    email = request.form.get('mail')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user check_password_hash(user.password, password):
+        flas('Coba cek username dan password anda')
+        return redirect(url_for('auth.login'))
+
+    return redirect(url_for('main.profile'))
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def sugnup_post():
+    email = request.from.get('email')
+    name = request.from.get('name')
+    password = request.from.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        flash('Email sudah ada, langsung ke login kau kimak!')
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
+@auth.route('/logout')
+def logout():
+    return 'Logout'
